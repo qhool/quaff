b49f4f0b4a22faf50f1aa120b308552491500ecb
Josh Burroughs <josh@qhool.com>
2014-03-12 21:05:06 -0400

    move meck to test dependency   * mix dep only in test env   * rebar.config.script checks REBAR_ENV env var   * update travis config   * make elixir version configurable with travix mix builds     and rebar script   * add elixir versions to test configs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Quaff.Mixfile do
   def project do
     [ app: :quaff,
       version: "0.0.1",
-      deps: deps,
+      deps: deps(Mix.env)
     ]
   end
 
@@ -12,10 +12,13 @@ defmodule Quaff.Mixfile do
     []
   end
 
-  defp deps do
+  defp deps(:test) do
+    [ { :meck,  git: "https://github.com/eproxus/meck.git", branch: "master" } |
+        deps(:prod) ]
+  end
+  defp deps(_) do
     [ { :aleppo, compile: "rebar compile",
         git: "https://github.com/ChicagoBoss/aleppo.git", branch: "master" },
-      { :meck,  git: "https://github.com/eproxus/meck.git", branch: "master" }
     ]
   end
 end
