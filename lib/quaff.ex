88a63786870f202ee720252ae28b1fceeb252c78
Michael Oliver <moliver@pinterest.com>
2015-08-14 11:59:59 -0700

    Updated to Elixir 1.0.5, removed rebar stuff

---
+++ lib/quaff.ex
@@ -0,0 +1,2 @@
+defmodule Quaff do
+end
--- mix.exs
+++ mix.exs
@@ -2,23 +2,32 @@ defmodule Quaff.Mixfile do
   use Mix.Project
 
   def project do
-    [ app: :quaff,
-      version: "0.0.1",
-      deps: deps(Mix.env)
-    ]
+    [app: :quaff,
+     version: "0.0.1",
+     elixir: "~> 1.0",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps]
   end
 
+  # Configuration for the OTP application
+  #
+  # Type `mix help compile.app` for more information
   def application do
     []
   end
 
-  defp deps(:test) do
-    [ { :meck,  git: "https://github.com/eproxus/meck.git", branch: "master" } |
-        deps(:prod) ]
-  end
-  defp deps(_) do
-    [ { :aleppo, compile: "rebar compile",
-        git: "https://github.com/ChicagoBoss/aleppo.git", branch: "master" },
-    ]
+  # Dependencies can be Hex packages:
+  #
+  #   {:mydep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #
+  # Type `mix help deps` for more examples and options
+  defp deps do
+    [{:aleppo, compile: "rebar compile", github: "ChicagoBoss/aleppo", branch: "master"},
+     {:meck, "~> 0.8.2"}]
   end
 end
