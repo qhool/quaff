8e9e88726bf05b316c58b3235e330578673ab445
Carlos Brito Lage <carlos@carloslage.net>
2018-04-05 16:57:34 +0100

    package def

---
+++ package.exs
@@ -0,0 +1,10 @@
+Expm.Package.new(
+  name: "quaff",
+  description: "Quaff is a set of tools for integrating Elixir into erlang applications (or vice versa).",
+  version: "0.0.2",
+  keywords: ["Elixir", "erlang", "tools", "utilities"],
+  licenses: [[name: "Apache License 2.0"]],
+  maintainers: [
+    [name: "Josh Burroughs",  email: "josh@qhool.com"],
+    [name: "Carlos Brito Lage",  email: "carlos@carloslage.net"]],
+  repositories: [[github: "aruki-delivery/quaff", branch: "master"]])
